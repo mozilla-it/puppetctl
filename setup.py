0588e4592f1680be78e24316c13e657e042388ee
Greg Cox <gcox@mozilla.com>
2022-10-31 21:53:25 +0000

    Relicense to Apache
    The LICENSE we added a few days ago was done without much thought.
    puppetctl falls under 'a stand-alone component that can be reused outside Mozilla products' and thus the pivot.

--- setup.py
+++ setup.py
@@ -1,7 +1,3 @@
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-# Copyright (c) 2020 Mozilla Corporation
 ''' Packaging script '''
 
 import os
@@ -54,7 +50,7 @@ setup(
         'setuptools',
         'six',
     ],
-    license='MPL',
+    license='Apache License 2.0',
     entry_points={
         'console_scripts': ['puppetctl=puppetctl.command_line:main'],
     },
@@ -64,6 +60,6 @@ setup(
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Topic :: Software Development :: Libraries :: Python Modules',
-        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+        'License :: OSI Approved :: Apache Software License',
     ],
 )
